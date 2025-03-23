8c3bcc172d3c0bd9cf4b449a55724fad821050f3
Martin Lowe <martinlowe673@gmail.com>
2025-03-23 18:57:59 +0000

    form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,27 +20,28 @@ const formSchema = z.object({
 })
 
 
-export function ProfileForm() {
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
-    })
-   
-    // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-      // Do something with the form values.
-      // ✅ This will be type-safe and validated.
-      console.log(values)
-    }
-  }
+
 
 
 const AuthForm = () => {
+    
+        // 1. Define your form.
+        const form = useForm<z.infer<typeof formSchema>>({
+          resolver: zodResolver(formSchema),
+          defaultValues: {
+            username: "",
+          },
+        })
+       
+        // 2. Define a submit handler.
+        function onSubmit(values: z.infer<typeof formSchema>) {
+          // Do something with the form values.
+          // ✅ This will be type-safe and validated.
+          console.log(values)
+        }
+      
   
-    return (
+        return(
         <div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -64,8 +65,10 @@ const AuthForm = () => {
           </form>
         </Form>
         </div>
-      )
-  
+        )
 }
+      
+    
+
 
 export default AuthForm
