27905b03619f8b82f8ad97f14ded659c829bbde6
Martin Lowe <martinlowe673@gmail.com>
2025-03-23 18:49:17 +0000

    start of zod

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client"
+
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
 
 const AuthForm = () => {
   return (
     <div>
-      
+      AuthForm
     </div>
   )
 }
